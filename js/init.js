48d3716d7bdaba731f52f141d0928ff91377e161
Roman Travnikov <travnikovrn@gmail.com>
2017-11-20 16:08:49 +0300

    add subscribe

--- js/init.js
+++ js/init.js
@@ -1,8 +1,24 @@
-(function($){
-  $(function(){
+(function($) {
+    $(function() {
 
-    $('.button-collapse').sideNav();
-    $('.carousel').carousel();
+        $('.button-collapse').sideNav();
+        $('.carousel').carousel();
 
-  }); // end of document ready
+        $('#sbmt').submit(function(e) {
+            e.preventDefault();
+            $.ajax({
+                url: 'http://apimail.ahoolee.io/api/subscribe',
+                type: 'post',
+                data: $('#sbmt').serialize(),
+                success: function() {
+                    var $toastContent = $('<a href="#contact" class="button" style="position: fixed; z-index: 9999; left: 48%; top: 35%;"><img src="img/details/a_medium.svg" class="angle left top">SUCCESS<img src="img/details/a_medium.svg" class="angle right bottom"></a>')
+                    // var $toastContent = $('<div style="position: fixed; z-index: 9999; right: 42%; top: 45%;">I am toast content</div>');
+                    Materialize.toast($toastContent, 4000);
+        
+                    // alert('SUCCESS');
+                }
+            });
+        });
+
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
