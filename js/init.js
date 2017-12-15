7a2329ad7a1309fcfc356550d3ceaf0a253163eb
PeterGashitsky <gashitsky@gmail.com>
2017-12-15 16:18:57 +0300

    js

--- js/init.js
+++ js/init.js
@@ -1,13 +1,25 @@
 (function($) {
     $(function() {
 
+    var $window = $(window);
+
+    function resize() {
+        if ($window.width() < 1280) {
+            $('#table').removeClass('table-of-contents');
+            $('#featured, #cases, #dapps, #mission, #roadmap, #rewards, #partner, #team, #advisors, #media, #contact').removeClass('scrollspy');
+        }
+    }
+
+    $window
+        .resize(resize)
+        .trigger('resize');
+
+        $(document).ready(function(){ $('.scrollspy').scrollSpy(); });
+
+
         $('.collapsible').collapsible({ expandable: true });
         $('.button-collapse').sideNav();
         $('#wlmodal').modal();
-        $(document).ready(function(){
-            $('.scrollspy').scrollSpy();
-        });
-
 
         $('#sbmt').submit(function(e) {
             $('#wlmodal').removeClass( 'disnone' ).addClass( 'disblock' );
@@ -37,8 +49,22 @@
         })
 
         $('#calltoaction').on('click', function() {
-            $('#calltoaction').addClass( 'disnone' );
+            $( "#youremail" ).focus();
         })
 
     });
 })(jQuery);
+
+var contact = $("#contact").offset();
+
+$(document).ready(function() {
+    $(window).scroll(function(){
+        var screenPosition = $(document).scrollTop();
+        if (screenPosition > contact.top) {
+            $( "#calltoaction" ).removeClass( 'disblock' ).addClass( 'disnone' );
+        }
+        if (screenPosition < contact.top) {
+            $( "#calltoaction" ).addClass( 'disblock' ).removeClass( 'disnone' );
+        }
+    });
+});
