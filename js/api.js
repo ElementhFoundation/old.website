fe7f9bf81a6b71bde37a855de9a96619307d4be1
ionix <ionixxx@gmail.com>
2018-01-27 17:35:59 +0300

    Personal Area

---
+++ js/api.js
@@ -0,0 +1,156 @@
+var user = null
+var isUserLoaded = false
+var apiUrl = 'https://api.elementh.io/'
+var crowdAddresses = null
+var isCrowdAddressesLoaded = null
+var web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/XQF4D1KpJZSQXYZOZxIX'))
+
+function getBalance (addrToken, addr, callback) {
+  var tknAddress = (addr).substring(2)
+  var contractData = ('0x70a08231000000000000000000000000' + tknAddress)
+  var balance = 0
+  if (addr) {
+    web3.eth.call({
+      to: addrToken,
+      data: contractData
+    }, function (err, result) {
+      if (result) {
+        balance = web3.utils.fromWei(result, 'ether')
+        callback(null, balance)
+      }
+      else {
+        callback(err, null)
+      }
+    })
+  }
+}
+
+function getUser (callback) {
+  if(!user && !isUserLoaded){
+    getProfile(function (err, data) {
+      if (err) {
+        callback(err, null)
+        return null
+      }
+      user = data
+      isUserLoaded = true
+      callback(null, user)
+    })
+  }else{
+    callback(null, user)
+  }
+}
+
+function getCrowdAddresses (callback) {
+  if(!crowdAddresses && !isCrowdAddressesLoaded){
+    getAddress(function (err, data) {
+      if (err) {
+        callback(err, null)
+        return null
+      }
+      crowdAddresses = data
+      isCrowdAddressesLoaded = true
+      callback(null, crowdAddresses)
+    })
+  }else{
+    callback(null, crowdAddresses)
+  }
+}
+
+function authFacebook (access_token, callback) {
+  makeApiRequest('auth/facebook', 'POST', {access_token}, callback)
+}
+
+function sendVerification (callback) {
+  makeApiRequest('sendVerification', 'GET', null, callback)
+}
+
+function sendRecovery (email, callback) {
+  makeApiRequest('sendRecovery', 'GET', {email}, callback)
+}
+
+function checkRecovery (code, callback) {
+  makeApiRequest('checkRecovery/email/' + code, 'GET', null, callback)
+}
+
+function checkVerification (code, callback) {
+  makeApiRequest('checkVerification/email/' + code, 'GET', null, callback)
+}
+
+function getAddress (callback) {
+  makeApiRequest('getAddress', 'GET', null, callback)
+}
+function setPassword (password, callback) {
+  makeApiRequest('setPassword', 'POST', {password}, callback)
+}
+
+function setWalletETH (data, callback) {
+  makeApiRequest('setWalletETH', 'POST', data, callback)
+}
+
+function setWalletBTC (data, callback) {
+  makeApiRequest('setWalletBTC', 'POST', data, callback)
+}
+
+function setRound (data, callback) {
+  makeApiRequest('setRound', 'POST', data, callback)
+}
+
+function setProfile (data, callback) {
+  makeApiRequest('setProfile', 'POST', data, callback)
+}
+
+function signUp (data, callback) {
+  makeApiRequest('signUp', 'POST', data, callback)
+}
+
+function signIn (data, callback) {
+  makeApiRequest('signIn', 'POST', data, callback)
+}
+
+function signOut (callback) {
+  makeApiRequest('signOut', 'POST', null, callback)
+}
+
+function getProfile (callback) {
+  makeApiRequest('getProfile', 'GET', null, callback)
+}
+
+function makeApiRequest (url, method, data, callback) {
+  var request = $.ajax({
+    url: apiUrl + url,
+    method: method,
+    dataType: 'json',
+    data: data,
+    tryCount: 0,
+    retryLimit: 3,
+    xhrFields: {
+      withCredentials: true
+    }
+  })
+  request.done(function (result) {
+    if (!result.response) {
+      callback('Empty response', null)
+      return
+    }
+    if (result.response.error) {
+      callback(result.response.error, null)
+      return
+    }
+    callback(null, result.response.result)
+  })
+
+  request.fail(function (jqXHR, textStatus) {
+    if (textStatus == 'timeout') {
+      this.tryCount++
+      if (this.tryCount <= this.retryLimit) {
+        //try again
+        $.ajax(this)
+      } else {
+        callback(textStatus, null)
+      }
+    } else {
+      callback(textStatus, null)
+    }
+  })
+}
--- js/init.js
+++ js/init.js
@@ -1,5 +1,13 @@
 $(function () {
 
+  var hash = window.location.hash.substr(1)
+  if(hash){
+    if(hash === 'emailVerified') {
+      $('#emailVerified').removeClass('disnone')
+      $('#emailVerified').append('<img height="1" width="1" style="display:none" src="https://matchico.com/track-investor/87c734a9-5afb-487d-9355-6bad7f725c30/signup.gif"/><img height="1" width="1" style="display:none" src="https://matchico.com/track-investor/87c734a9-5afb-487d-9355-6bad7f725c30/signup.gif"/>')
+    }
+  }
+
   var $window = $(window);
 
   function resize () {
@@ -9,18 +17,11 @@ $(function () {
     }
   }
 
-  $window
-    .resize(resize)
-    .trigger('resize');
+  $window.resize(resize).trigger('resize');
 
   $('.scrollspy').scrollSpy();
 
-  $('.collapsible').collapsible({expandable: true});
-  $('.button-collapse').sideNav();
-  $('#wlmodal').modal();
-
   $('#sbmt').submit(function (e) {
-    $('#wlmodal').removeClass('disnone').addClass('disblock');
     e.preventDefault();
     $.ajax({
       url: 'https://apimail.ahoolee.io/api/subscribe',
@@ -28,81 +29,6 @@ $(function () {
       data: $('#sbmt').serialize()
     });
   });
-  $('#whitelist').submit(function (e) {
-    $('#whitelist input[name=email]').val($('#sbmt input[name=email]').val());
-    e.preventDefault();
-    $('#loading').addClass('disblock').removeClass('disnone');
-    $.ajax({
-      url: 'https://apimail.ahoolee.io/api/invest',
-      type: 'post',
-      data: $('#whitelist').serialize(),
-      success: function () {
-        $('#loading').removeClass('disblock').addClass('disnone');
-        $('#wlmodal').removeClass('disblock').addClass('disnone');
-        $('#wl_ps_ok').addClass('disblock').removeClass('disnone');
-      },
-      error: function () {
-        $('#loading').removeClass('disblock').addClass('disnone');
-        $('#wl_ps_error').addClass('disblock').removeClass('disnone');
-      }
-    });
-  });
-
-  $('#wlmodalclose').on('click', function () {
-    $('#wlmodal').removeClass('disblock').addClass('disnone');
-  })
-
-  $('#wallet').submit(function (e) {
-    e.preventDefault();
-    $.ajax({
-      url: 'https://apimail.ahoolee.io/api/presale?wallet=' + $('#yourwallet').val().trim(),
-      type: 'get',
-      dataType: 'json'
-    }).done(
-      function (data) {
-        if (data.response.status == 'Error') {
-          $('#wallet_error').addClass('disblock').removeClass('disnone');
-          $('#wallet_error_msg').html(data.response.message);
-        }
-
-        if (data.response.status == 'Ok') {
-          if ($('#yourwallet').val().trim().length > 26 && $('#yourwallet').val().trim().length < 35) {
-            // btc
-            $('#wallet_btc_ok').addClass('disblock').removeClass('disnone');
-            $('#address_btc_div').html(data.response.btc);
-            $('#address_btc_form').html(data.response.btc);
-          }
-
-          else if ($('#yourwallet').val().trim().length > 39 && $('#yourwallet').val().trim().length < 45) {
-            // eth
-            $('#wallet_eth_ok').addClass('disblock').removeClass('disnone');
-            $('#address_eth_div').html(data.response.eth);
-            $('#address_eth_form').html(data.response.eth);
-
-          }else{
-            $('#wallet_error').addClass('disblock').removeClass('disnone');
-            $('#wallet_error_msg').html('wrong address length');
-          }
-        }
-
-      }).fail(function () {
-        $('#wallet_error').addClass('disblock').removeClass('disnone');
-        $('#wallet_error_msg').html('Connection error! Please try again.');
-      }
-    );
-  });
-
-  $('#wallet_err_close').on('click', function () {
-    $('#wallet_error').removeClass('disblock').addClass('disnone');
-  })
-
-  $('#wallet_eth_ok_close').on('click', function () {
-    $('#wallet_eth_ok').removeClass('disblock').addClass('disnone');
-  })
-
-  $('#wallet_btc_ok_close').on('click', function () {
-    $('#wallet_btc_ok').removeClass('disblock').addClass('disnone');
-  })
 
   var clipboardEth = new Clipboard('#wallet_eth_copy');
   clipboardEth.on('success', function (e) {
@@ -136,65 +62,6 @@ $(function () {
     $('#wallet_btc_copy_ok').addClass('visible');
   })
 
-  $('#join_wl').on('click', function () {
-    $('#wl_ps').addClass('disblock').removeClass('disnone');
-  })
-
-  $('#wl_ps_close').on('click', function () {
-    $('#wl_ps').removeClass('disblock').addClass('disnone');
-  })
-
-  $('#wl_ps_form').submit(function (e) {
-    $('#loading').addClass('disblock').removeClass('disnone');
-    e.preventDefault();
-    $.ajax({
-      url: 'https://apimail.ahoolee.io/api/invest',
-      type: 'post',
-      data: $('#wl_ps_form').serialize(),
-      success: function () {
-        $('#loading').removeClass('disblock').addClass('disnone');
-
-        $('#wl_ps_ok').addClass('disblock').removeClass('disnone');
-      },
-      error: function () {
-        $('#loading').removeClass('disblock').addClass('disnone');
-        $('#wl_ps_error').addClass('disblock').removeClass('disnone');
-      }
-    });
-  });
-
-  $('#wl_ps_ok_close').on('click', function () {
-    $('#wl_ps_ok').removeClass('disblock').addClass('disnone');
-    $('#wl_ps').removeClass('disblock').addClass('disnone');
-
-    if ($('#radio_presale').prop("checked")) {
-      if ($('#radio_eth').prop("checked")) {
-        if ($('#ETH_wallet').length && $('#yourwallet').length) {
-          $('#yourwallet').val($('#ETH_wallet').val())
-          if($('#buttonWallet').length) {
-            $('#buttonWallet').click()
-          }
-        }else {
-          window.location.href = "presale.html#" + $('#mainPageWallet').val();
-        }
-      }
-
-      if ($('#radio_btc').prop("checked")) {
-        if ($('#BTC_wallet').length && $('#yourwallet').length) {
-          $('#yourwallet').val($('#BTC_wallet').val())
-          if($('#buttonWallet').length) {
-            $('#buttonWallet').click()
-          }
-        }else{
-          window.location.href = "presale.html#" + $('#mainPageWallet').val();
-        }
-      }
-    }
-  })
-  $('#wl_ps_error_close').on('click', function () {
-    $('#wl_ps_error').removeClass('disblock').addClass('disnone');
-  })
-
   $('input[name=currency]:radio').change(function () {
 
     if ($('input[name=currency]:checked').val() == 'eth') {
@@ -210,18 +77,9 @@ $(function () {
       url: 'https://ethgasstation.info/json/ethgasAPI.json',
       dataType: 'json',
       success: function (data) {
-        console.log(data)
         $('#gasprice').html(Math.round(data.average / 10))
-
       }
     });
-  }
-  var hash = window.location.hash.substr(1)
-  if(hash){
-    if($('#buttonWallet').length) {
-      $('#yourwallet').val(hash)
-      $('#buttonWallet').click()
-    }
   }
   if ($('#timer').length) {
     var curDate = new Date().getTime();
@@ -233,7 +91,6 @@ $(function () {
       countDownDate = 1517443200000;
       $('#timerTitle').html('50% bonus at Private Pre-Sale round ends in:')
       $('#presaleLink').show(0)
-      $('#contactLink').hide(0)
     }
 
     var x = setInterval(function () {
@@ -268,8 +125,277 @@ $(function () {
         $('#timerTitle').html('50% bonus at Private Pre-Sale round ends in:')
         countDownDate = 1517443200000;
         $('#presaleLink').show(0)
-        $('#contactLink').hide(0)
       }
     }, 1000);
   }
+
+  if ($('#userblock').length) {
+    getUser(function (err, data) {
+      if (data) {
+        $('#userblockR').removeClass('disnone')
+      } else {
+        $('#userblockU').removeClass('disnone')
+      }
+    })
+  }
+
+  getCrowdAddresses(function (err, data) {
+    $('#address_eth_div').html(data.eth)
+    $('#address_eth_form').val(data.eth)
+    $('#address_btc_div').html(data.btc)
+    $('#address_btc_form').val(data.btc)
+    //var cnt = getContract(data.eth)
+    if ($('#eth_qrcode').length) {
+      new QRCode(document.getElementById("eth_qrcode"),
+        {
+          text: 'ethereum:' + data.eth,
+          width: 128,
+          height: 128,
+          colorDark: "#FFFFFF",
+          colorLight: "#000000",
+          correctLevel: QRCode.CorrectLevel.H
+        })
+      new QRCode(document.getElementById("btc_qrcode"), {
+        text: 'bitcoin:' + data.btc,
+        width: 128,
+        height: 128,
+        colorDark: "#FFFFFF",
+        colorLight: "#000000",
+        correctLevel: QRCode.CorrectLevel.H
+      })
+    }
+    getUser(function (err, data2) {
+      if(data2 && data2.wallet_eth) {
+        getBalance(data.tokenAddress, data2.wallet_eth, function (err, data) {
+          if (data) {
+            $('#user_token').html(data + ' EEE')
+          }
+        })
+      }
+    })
+  })
+
+  $('#get_eee_btc').on('click', function () {
+    $('#wallet_btc_ok').removeClass('disnone')
+  })
+
+  $('#wallet_btc_ok_close').on('click', function () {
+    $('#wallet_btc_ok').addClass('disnone')
+  })
+
+  $('#get_eee_eth').on('click', function () {
+    $('#wallet_eth_ok').removeClass('disnone')
+  })
+
+  $('#wallet_eth_ok_close').on('click', function () {
+    $('#wallet_eth_ok').addClass('disnone')
+  })
+
+  var signup_form = $('#signup_form')
+  if (signup_form.length) {
+    getUser(function (err, data) {
+      if (data) {
+        window.location.href = "/profile"
+      }
+    })
+    signup_form.submit(function (e) {
+      e.preventDefault();
+      signup_form.find('.error').addClass('disnone')
+      signUp($(this).serialize(), function (err, data) {
+        if (err) {
+          signup_form.find('.error').html(err).removeClass('disnone')
+        } else {
+          window.location.href = "/profile"
+        }
+      })
+    })
+  }
+
+  var signin_form = $('#signin_form')
+  if (signin_form.length) {
+    getUser(function (err, data) {
+      if (data) {
+        window.location.href = "/profile"
+      }
+    })
+    signin_form.submit(function (e) {
+      e.preventDefault();
+      signin_form.find('.error').addClass('disnone')
+      signIn($(this).serialize(), function (err, data) {
+        if (err) {
+          signin_form.find('.error').html(err).removeClass('disnone')
+        } else {
+          window.location.href = "/profile"
+        }
+      })
+    })
+  }
+
+  var wallet_eth_edit_form = $('#wallet_eth_edit_form')
+  if (wallet_eth_edit_form.length) {
+    wallet_eth_edit_form.submit(function (e) {
+      e.preventDefault();
+      setWalletETH($(this).serialize(), function (err, data) {
+        location.reload()
+      })
+    })
+  }
+
+  var wallet_btc_edit_form = $('#wallet_btc_edit_form')
+  if (wallet_btc_edit_form.length) {
+    wallet_btc_edit_form.submit(function (e) {
+      e.preventDefault();
+      setWalletBTC($(this).serialize(), function (err, data) {
+        location.reload()
+      })
+    })
+  }
+
+  var user_round_edit_form = $('#user_round_edit_form')
+  if (user_round_edit_form.length) {
+    user_round_edit_form.submit(function (e) {
+      e.preventDefault();
+      console.log($(this).serialize())
+      setRound($(this).serialize(), function (err, data) {
+        location.reload()
+      })
+    })
+  }
+
+  var profile_data = $('#profile_data')
+  if (profile_data.length) {
+    profile_data.submit(function (e) {
+      e.preventDefault();
+      profile_data.find('.error').addClass('disnone')
+      setProfile($(this).serialize(), function (err, data) {
+        if (err) {
+          profile_data.find('.error').html(err).removeClass('disnone')
+        } else {
+          location.reload()
+        }
+      })
+    })
+  }
+
+  var profile = $('#profile')
+  if (profile.length) {
+    getUser(function (err, data) {
+      if (err || !data) {
+        window.location.href = "/signup"
+      } else {
+        if (!data.verified) {
+          $('#verification').removeClass('disnone')
+        }else{
+          $('#wallet_data').removeClass('disnone')
+        }
+
+        if (data.email) {
+          $('#email').val(data.email).prop("readonly", true)
+        }
+
+        if (data.round) {
+          if(data.partnerUrl) {
+            $('#user_ref').html(data.partnerUrl)
+          }
+          if(data.round == 1){
+            $('#user_round').html('Private Pre-Sale')
+          }
+          if(data.round == 2){
+            $('#user_round').html('Pre-ICO')
+          }
+          if(data.round == 3){
+            $('#user_round').html('ICO')
+          }
+
+          if(data.whitelist){
+            $('#user_round').append('<span class="green"> (You are in whitelist)</span>')
+          }
+
+
+          if (data.wallet_btc) {
+            $('#user_wallet_btc').html(data.wallet_btc)
+          } else {
+            $('#user_wallet_btc').html('none')
+          }
+
+          if (data.wallet_eth) {
+            $('#user_wallet_eth').html(data.wallet_eth)
+          } else {
+            $('#user_wallet_eth').html('none')
+          }
+          $('#profile_data_complete').removeClass('disnone')
+        } else {
+          $('#profile_data').removeClass('disnone')
+          $('#profile_data_fill').removeClass('disnone')
+        }
+      }
+    })
+  }
+
+  var resetpass_form = $('#resetpass_form')
+  if (resetpass_form.length) {
+    resetpass_form.submit(function (e) {
+      resetpass_form.find('.error').addClass('disnone')
+      resetpass_form.find('.allok').addClass('disnone')
+      e.preventDefault();
+      sendRecovery($('#email').val(),function (err, data) {
+        if (err) {
+          resetpass_form.find('.error').html(err).removeClass('disnone')
+        } else {
+          resetpass_form.find('.allok').removeClass('disnone')
+        }
+      })
+    })
+    signin_form.submit(function (e) {
+      e.preventDefault();
+      signin_form.find('.error').addClass('disnone')
+      signIn($(this).serialize(), function (err, data) {
+        if (err) {
+          signin_form.find('.error').html(err).removeClass('disnone')
+        } else {
+          window.location.href = "profile.html"
+        }
+      })
+    })
+  }
+  $('.edit').on('click', function () {
+    var parent = $(this).parent()
+    parent.addClass('disnone')
+    parent.parent().find('span:first-child').addClass('disnone')
+    parent.parent().find('form').removeClass('disnone')
+    parent.parent().find('form').find('input').focus()
+  })
+
+  $('.done').on('click', function () {
+    var closest = $(this).closest('form')
+    closest.submit()
+  })
+
+  $('.cancel').on('click', function () {
+    var closest = $(this).closest('form')
+    closest.addClass('disnone')
+    closest.parent().find('span:first-child').removeClass('disnone')
+    closest.parent().children('.line_icon').removeClass('disnone')
+  })
+
+  $('#logout_link').on('click', function () {
+    signOut(function (err, data) {
+      window.location.href = "/"
+    })
+  })
 });
+
+
+function checkLoginState () {
+  FB.getLoginStatus(function (response) {
+    if (response.authResponse && response.authResponse.accessToken) {
+      authFacebook(response.authResponse.accessToken, function (err, data) {
+        if (err) {
+          $('.error').html(err).removeClass('disnone')
+        } else {
+          window.location.href = "profile.html"
+        }
+      })
+    }
+  });
+}
