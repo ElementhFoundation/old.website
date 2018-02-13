e5ef0a5e99d5f4598cac71c4f9dcce57af592255
ionix <ionixxx@gmail.com>
2018-02-14 02:49:21 +0300

    pre-ico end

--- js/init.js
+++ js/init.js
@@ -1,4 +1,3 @@
-var countDownDate = 1518566400000
 var btcWalllet = null
 var balance = 0
 var user = null
@@ -213,55 +212,6 @@ function init () {
         $('#gasprice').html(Math.round(data.average / 10))
       }
     });
-  }
-  if ($('#timer').length) {
-    var curDate = new Date().getTime();
-
-    if (curDate < countDownDate) {
-      $('#timerTitle').html('Pre-ICO round end in:')
-      $('#preico').removeClass('disnone')
-    } else {
-      countDownDate = null;
-      $('#timerTitle').html('ICO coming soon')
-    }
-
-    if (countDownDate) {
-      var x = setInterval(function () {
-
-        // Get todays date and time
-        var now = new Date().getTime();
-
-        // Find the distance between now an the count down date
-        var distance = countDownDate - now;
-
-        // Time calculations for days, hours, minutes and seconds
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-        if (hours < 10) {
-          hours = '0' + hours
-        }
-        if (minutes < 10) {
-          minutes = '0' + minutes
-        }
-        if (seconds < 10) {
-          seconds = '0' + seconds
-        }
-        // Display the result in the element with id="demo"
-
-        $('#timer').html(days + "d " + hours + ":" + minutes + ":" + seconds)
-        // If the count down is finished, write some text
-        if (distance <= 0) {
-
-          $('#timerTitle').html('ICO coming soon')
-          countDownDate = null;
-          clearInterval(x)
-          $('#preico').removeClass('disnone')
-        }
-      }, 1000);
-    }
   }
 
   $('#get_eee_btc').on('click', function () {
